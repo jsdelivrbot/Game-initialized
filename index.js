ea0c8039b8254eddd03f6f3e63c5fb881476197c
idkondor <odis.kondor@gmail.com>
2017-09-04 23:47:16 +0300

    add app.get function rewrite address

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/file', function(request, response) {
   response.send('Hello World!')
 })
 
@@ -15,7 +15,7 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-app.get('/file', function(request, response){
+app.get('/', function(request, response){
   response.sendfile('ComputerGeek.html');
 });
 
