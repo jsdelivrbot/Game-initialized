c64994d1779d742eb65751977e639faf2faded22
idkondor <odis.kondor@gmail.com>
2017-09-04 23:44:39 +0300

    add app.get function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var http = require("http");
+const fs = require('fs');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -11,3 +14,15 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+app.get('/file', function(request, response){
+  response.sendfile('ComputerGeek.html');
+});
+
+http.createServer(function(req, res){
+    fs.readFile('ComputerGeek.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(3500);
